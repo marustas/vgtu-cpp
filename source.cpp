07a5b14ad2975cc83b28d97216d545721313704a
Stanislau Marudau <marudov.stas@gmail.com>
2022-03-02 18:35:49 +0200

    code comments added

--- source.cpp
+++ source.cpp
@@ -1,14 +1,12 @@
-#include<iostream>
+#include <iostream>
 #include "Date.h"
 using namespace std;
 
 int main()
 {
-   const Date cd{15, Month::feb, 2022};
-   Date dd{31,Month::dec,2021};
-   // cout << d.getYear() << "/" << d.getMonth() << "/" << d.getDay() << endl;
-    cd.print();
-    dd.print();
-    dd.addDay();
-    dd.print();
+    // Date dd{31, Month::dec, 2021};
+    // // cout << d.getYear() << "/" << d.getMonth() << "/" << d.getDay() << endl;
+    // dd.print();
+    // dd.addDay();
+    // dd.print();
 }
