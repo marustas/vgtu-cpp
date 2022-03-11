40224d251430cdd63c1e4c65064e0393deaf8af1
Stanislau Marudau <marudov.stas@gmail.com>
2022-03-11 17:31:29 +0200

    lw2_task6 solved, configurations for cpp updated

---
+++ lw2_task6.cpp
@@ -0,0 +1,27 @@
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+bool palindrome(const vector<int> &);
+
+int main()
+{
+    int x;
+    vector<int> values;
+    cout << "enter your numbers one at a time(end with a non-digit):\n";
+    while (cin >> x)
+        values.push_back(x);
+
+    for (int v : values)
+        cout << v << ' ';
+
+    if (palindrome(values))
+        cout << " is a palindrome.\n";
+    else
+        cout << "is not a palindrome.\n";
+}
+
+bool palindrome(const vector<int> &vect)
+{
+    return equal(vect.begin(), vect.begin() + vect.size() / 2, vect.rbegin());
+}
