8b5560cf723d48cf590d40502ce88f75a8a84620
Stanislau Marudau <marudov.stas@gmail.com>
2022-03-12 17:03:11 +0200

    lw2 task1 improved

--- labwork2/lw2_task1.cpp
+++ labwork2/lw2_task1.cpp
@@ -1,43 +1,26 @@
 #include <iostream>
+#include <iomanip>
 #include <vector>
 using namespace std;
-
-// Defining the rows and columns of
-// vector of vectors
-#define COL 10
-
 int main()
 {
-    int x;
-    cout << "Enter the upper bound:" << endl;
-    cin >> x;
-    int row = x / 10;
-    // Initializing the vector of vectors
-    vector<vector<int> > vec;
+    cout << "Enter the upper bound: ";
+    unsigned bound{};
+    cin >> bound;
 
-    // Elements to insert in column
-    int num = 1;
+    std::vector<unsigned> values;
+    for (unsigned i{1}; i <= bound; ++i)
+        values.push_back(i);
 
-    // Inserting elements into vector
-    for (int i = 0; i <= row; i++)
+    size_t count{};
+    size_t perline{10};
+    for (auto value : values)
     {
-        // Vector to store column elements
-        vector<int> v1;
-        for (int j = 1; j <= 10; j++)
-        {
-            v1.push_back(num);
-            num += 1;
-        }
-        // Pushing back above 1D vector
-        // to create the 2D vector
-        vec.push_back(v1);
+        if (value % 7 == 0 || value % 13 == 0)
+            continue;
+        cout << setw(6) << value;
+        if (++count % perline == 0)
+            cout << "\n";
     }
-    // Displaying the 2D vector
-    for (int i = 0; i < vec.size(); i++)
-    {
-            for (int j = 0; j < vec[i].size(); j++)
-                cout << vec[i][j] << " ";
-        cout << endl;
-    }
-    return 0;
+    cout << endl;
 }
